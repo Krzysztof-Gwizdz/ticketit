65796f51cd83f67390d5db65fbaa2e76175f5ae1
KrzysztofG <krzysztof.gwizdz11@gmail.com>
2021-01-06 13:40:35 +0100

    Refactor Authority entity class

--- src/main/java/pl/krzysztofgwizdz/ticketit/entity/Authority.java
+++ src/main/java/pl/krzysztofgwizdz/ticketit/entity/Authority.java
@@ -11,12 +11,12 @@ public class Authority {
     @Id
     @Column(name = "authorityid")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
 
-    @Column(name = "authority_name", unique = true)
+    @Column(name = "authority_name", unique = true, nullable = false)
     private String authorityName;
 
-    @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST}, fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(
             name = "Users_Authorities",
             joinColumns = {@JoinColumn(name = "authorityid")},
@@ -33,6 +33,14 @@ public class Authority {
     public Authority(String authorityName) {
         this.authorityName = authorityName;
         this.userList = new ArrayList<>();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getAuthorityName() {
