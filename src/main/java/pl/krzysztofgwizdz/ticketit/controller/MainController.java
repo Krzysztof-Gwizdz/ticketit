ed47896334b4aabf6e97c1955e97124f9d660f4f
kgwizdz <krzysztof.gwizdz11@gmail.com>
2021-03-14 18:38:42 +0100

    Add security section to navigation, cleanup in main controller, add thymeleaf security extras

--- src/main/java/pl/krzysztofgwizdz/ticketit/controller/MainController.java
+++ src/main/java/pl/krzysztofgwizdz/ticketit/controller/MainController.java
@@ -1,15 +1,13 @@
 package pl.krzysztofgwizdz.ticketit.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/")
-    public String homePage(Model model) {
-        model.addAttribute("message", "Bajur!");
+    public String homePage() {
         return "main/home";
     }
 }
