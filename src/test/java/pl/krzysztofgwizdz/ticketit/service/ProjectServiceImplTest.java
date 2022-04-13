5837fa56ae27aac160f9a973e72f0754dccbeea8
kgwizdz <krzysztof.gwizdz11@gmail.com>
2022-04-13 20:48:17 +0200

    Add sqls

--- src/test/java/pl/krzysztofgwizdz/ticketit/service/ProjectServiceImplTest.java
+++ src/test/java/pl/krzysztofgwizdz/ticketit/service/ProjectServiceImplTest.java
@@ -1,4 +1,30 @@
 package pl.krzysztofgwizdz.ticketit.service;
 
+import org.mockito.Mock;
+import pl.krzysztofgwizdz.ticketit.dao.ProjectRepository;
+import pl.krzysztofgwizdz.ticketit.dao.ProjectRoleRepository;
+import pl.krzysztofgwizdz.ticketit.dao.UserRepository;
+import pl.krzysztofgwizdz.ticketit.entity.Project;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 public class ProjectServiceImplTest {
+
+    @Mock
+    private ProjectRepository mockProjectRepository;
+
+    @Mock
+    private UserRepository mockUserRepository;
+
+    @Mock
+    private ProjectRoleRepository mockProjectRoleRepository;
+
+    @Mock
+    private Project mockProject;
+
+    public void setUp() {
+        when(mockProjectRepository.findByAcronym(any(String.class))).thenReturn(mockProject);
+        ProjectService projectService = new ProjectServiceImpl(mockProjectRepository, mockUserRepository, mockProjectRoleRepository);
+    }
 }
